55b3fb123361beacf45d299942aa619bd7bdec3e
PiyushGrubpac <jha.piyush@grubpac.com>
2026-05-26 10:30:57 +0530

    feat: add Docker support with Dockerfile and docker-compose configuration

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 export const API_CONFIG = {
 	BASE_URL:
 		process.env.NEXT_PUBLIC_API_BASE_URL ||
-		"http://43.204.34.10:8000/api/v1",
+		"https://43.204.34.10.nip.io/api/v1",
 };
 
 // API Endpoints
